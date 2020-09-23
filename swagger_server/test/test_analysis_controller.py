43bc1b360aec5c39c34c420453335f9b39b0aee7
Ashish Ahiwale <ahiwaleashish1995@gmail.com>
2020-09-23 12:28:05 -0400

    demo

---
+++ swagger_server/test/test_analysis_controller.py
@@ -0,0 +1,32 @@
+# coding: utf-8
+
+from __future__ import absolute_import
+
+from flask import json
+from six import BytesIO
+
+from swagger_server.models.result import Result  # noqa: E501
+from swagger_server.test import BaseTestCase
+
+
+class TestAnalysisController(BaseTestCase):
+    """AnalysisController integration test stubs"""
+
+    def test_get_location(self):
+        """Test case for get_location
+
+        Calculate
+        """
+        body = '\"The brown fox jumped over the brown log.\"'
+        response = self.client.open(
+            '/HelloEarth/Concordancenew/1.0.0/locate',
+            method='POST',
+            data=json.dumps(body),
+            content_type='text/plain')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
